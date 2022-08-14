9b3da2ebc6157e66b45e629754dda15b8d5399f5
Kota <maedakoutaa@gmail.com>
2022-08-14 10:08:41 +0900

    Initial Commit

---
+++ MiniApp79-SwiftUI-TransitionView/MiniApp79_SwiftUI_TransitionViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp79_SwiftUI_TransitionViewApp.swift
+//  MiniApp79-SwiftUI-TransitionView
+//
+//  Created by 前田航汰 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp79_SwiftUI_TransitionViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
